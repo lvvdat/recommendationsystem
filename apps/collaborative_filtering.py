5e532501b8487833ab14033e836a84688510c994
lvvdat <lvvdat@gmail.com>
2023-04-14 14:00:11 +0700

    update code

--- apps/collaborative_filtering.py
+++ apps/collaborative_filtering.py
@@ -1,7 +1,6 @@
 ﻿import numpy as np
 import pandas as pd
 
-import pickle
 import requests
 import streamlit as st
 from PIL import Image
@@ -9,43 +8,48 @@ from PIL import Image
 @st.cache_data
 def load_data():
     def convert_array_int(x):
-        x=x[x.index("[")+1:]
-        x=x[:x.index("]")]
-        x=x.split(',') 
-        x=[int(item) for item in x]
+        x = x[x.index("[")+1:]
+        x = x[:x.index("]")]
+        x = x.split(',') 
+        x = [int(item) for item in x]
         return x
 
     def convert_array_float(x):
         import regex
-        x=x[x.index("[")+1:]
-        x=x[:x.index("]")]
-        if x.find(',')>-1:
-            x=x.split(',')
-            x=[float(item) for item in x]
+        x = x[x.index("[")+1:]
+        x = x[:x.index("]")]
+        if x.find(',') > -1:
+            x = x.split(',')
+            x = [float(item) for item in x]
         else:
-            x=x.replace("\n"," ")
-            x=regex.sub(r'\s+', ' ', x).strip()
-            x=x.split(' ')
-            x=[float(item) for item in x]
+            x = x.replace("\n"," ")
+            x = regex.sub(r'\s+', ' ', x).strip()
+            x = x.split(' ')
+            x = [float(item) for item in x]
         return x
 
     gensim = pd.read_csv('data/Products_ThoiTrangNam_resultGensim.csv', index_col=0, converters={"suggestion": convert_array_int,"rate_suggestion":convert_array_float})
     gensim=gensim.drop(['link','text_clean_stopword'],axis=1)
     data=pd.read_csv('data/Products_ThoiTrangNam_resultALS.csv')
     data2=pd.read_csv('data/Products_ThoiTrangNam_rating_raw.csv',sep="\t")
+
     return data,gensim,data2
 
 def display_similar_products(df_filter):
     similar = len(df_filter)
+
     with st.container():
         k = 0
         col_count = 3
+
         for i in range(int(round(similar/col_count+0.5,0))):
             columns = st.columns(col_count)
+
             for column in range(col_count):
                 k = k + 1
                 product = df_filter.iloc[column + col_count*i]
-                cell = columns[column]                
+                cell = columns[column]      
+                
                 try:
                     image_url = product['image']
                     if image_url == np.nan:
@@ -54,7 +58,9 @@ def display_similar_products(df_filter):
                         cell.image(Image.open(requests.get(image_url, stream=True).raw).resize((150,150)))
                 except:
                     cell.image(Image.open('images/image-not-found-icon.png').resize((150,150)))
+
                 col1, col2 = cell.columns(2)
+
                 with col1:
                     try:
                         st.write("{:,}₫".format(int(product['price'])))
@@ -62,8 +68,10 @@ def display_similar_products(df_filter):
                         st.write(" ")
                 with col2:
                     st.write(str(product['rating'])+"★")
+
                 cell.write(product['product_name'])
                 cell.write(" ")
+
                 if k==similar:
                     break
             if k==similar:
@@ -74,10 +82,12 @@ def app():
     data, gensim,data2 = load_data()
     user_id=data.user_id.sort_values().unique() 
     customer_id = st.number_input('Type User ID',min_value=min(user_id),max_value=max(user_id))
+
     if not(customer_id in user_id):
         st.warning('User Id is not correct!', icon="⚠️")  
     else:
         st.success(str(data2[data2.user_id==customer_id]['user'].head(1).values[0])+' has successfully logged in') 
+
     with st.expander("Setting"):
         similar = st.slider('Select the maximum number of products similar to the above that you want the system to recommend (from 1 to 50)', 1, 25, 9)
         rating = st.slider('Select the minimum number of ratings similar to the above that you want the system to recommend (from 1 to 10)', 0, 10, 1)
@@ -90,6 +100,7 @@ def app():
         recommendlist=recommendlist[recommendlist.rating>=(0.5*rating)].head(similar)
         recommendlist['index_product']=recommendlist['recommend'].map(lambda x: gensim[gensim.product_id==x].head(1).index[0] if gensim[gensim.product_id==x].shape[0]>0 else np.NaN)
         recommendlist.dropna(inplace=True)
+
         if recommendlist.shape[0]==0:
             st.warning('No products were found according to the settings!', icon="⚠️")
         else:
--- apps/content_based_filtering.py
+++ apps/content_based_filtering.py
@@ -15,29 +15,28 @@ from gensim import similarities
 @st.cache_data
 def load_data():
     def convert_array_int(x):
-        x=x[x.index("[")+1:]
-        x=x[:x.index("]")]
-        x=x.split(',') 
-        x=[int(item) for item in x]
+        x = x[x.index("[")+1:]
+        x = x[:x.index("]")]
+        x = x.split(',') 
+        x = [int(item) for item in x]
         return x
 
     def convert_array_float(x):
         import regex
-        x=x[x.index("[")+1:]
-        x=x[:x.index("]")]
-        if x.find(',')>-1:
-            x=x.split(',')
-            x=[float(item) for item in x]
+        x = x[x.index("[")+1:]
+        x = x[:x.index("]")]
+        if x.find(',') > -1:
+            x = x.split(',')
+            x = [float(item) for item in x]
         else:
-            x=x.replace("\n"," ")
-            x=regex.sub(r'\s+', ' ', x).strip()
-            x=x.split(' ')
-            x=[float(item) for item in x]
+            x = x.replace("\n"," ")
+            x = regex.sub(r'\s+', ' ', x).strip()
+            x = x.split(' ')
+            x = [float(item) for item in x]
         return x
 
     gensim = pd.read_csv('data/Products_ThoiTrangNam_resultGensim.csv', index_col=0, converters={"suggestion": convert_array_int,"rate_suggestion":convert_array_float})
-    #gensim.dropna(inplace=True)
-    gensim=gensim.drop(['link','text_clean_stopword'],axis=1)
+    gensim=gensim.drop(['link', 'text_clean_stopword'], axis=1)
     gensim.reset_index(drop=True, inplace=True)
 
     return gensim
@@ -46,31 +45,39 @@ def load_data():
 def load_model():
     with open('files/vietnamese-stopwords.txt', 'r', encoding="utf8") as file:
         stopwords = file.read().split('\n')
+    
     tfidf = pickle.load(open("models/tfidf.mdl", "rb"))
     dictionary = Dictionary.load('models/dictionary.mdl')
     indexes = similarities.SparseMatrixSimilarity.load('models/SparseMatrixSimilarity.mdl')
+    
     return tfidf, dictionary, indexes, stopwords
 
 def display_similar_products(df_filter):
     similar = len(df_filter)
+
     with st.container():
         k = 0
         col_count = 3
-        for i in range(int(round(similar/col_count+0.5,0))):
+
+        for i in range(int(round(similar/col_count+0.5, 0))):
             columns = st.columns(col_count)
+
             for column in range(col_count):
                 k = k + 1
                 product = df_filter.iloc[column + col_count*i]
-                cell = columns[column]                
+                cell = columns[column]
+
                 try:
                     image_url = product['image']
                     if image_url == np.nan:
                         cell.image(Image.open('images/image-not-found-icon.png').resize((150,150)))
                     else:
-                        cell.image(Image.open(requests.get(image_url, stream=True).raw).resize((150,150)))
+                        cell.image(Image.open(requests.get(image_url, stream = True).raw).resize((150,150)))
                 except:
                     cell.image(Image.open('images/image-not-found-icon.png').resize((150,150)))
+                
                 col1, col2 = cell.columns(2)
+                
                 with col1:
                     try:
                         st.write("{:,}₫".format(int(product['price'])))
@@ -78,8 +85,10 @@ def display_similar_products(df_filter):
                         st.write(" ")
                 with col2:
                     st.write(str(product['rating'])+"★")
+                
                 cell.write(product['product_name'])
                 cell.write(" ")
+
                 if k==similar:
                     break
             if k==similar:
@@ -90,24 +99,30 @@ def app():
     gensim = load_data()
     tfidf, dictionary, indexes, stopwords = load_model()
     filters = st.radio('Please choose a method:', options=('Select product from the list', 'Enter the product description to search'), index=0)
+
     if filters == 'Select product from the list':
         col1, col2 = st.columns(2)
+        
         with col1:
             option1 = st.selectbox("Select category",options=gensim['sub_category'].unique())
         with col2:
-            option2 = st.selectbox("Select product",options=gensim[gensim.sub_category==option1]['product_name'])
-        product = gensim[(gensim['product_name'] == option2) &(gensim['sub_category'] == option1)]    
+            option2 = st.selectbox("Select product",options = gensim[gensim.sub_category == option1]['product_name'])
+        
+        product = gensim[(gensim['product_name'] == option2) & (gensim['sub_category'] == option1)]    
         selected_id = product['product_id'].values[0]
-        col1,col2=st.columns([1,3])
+        col1, col2 = st.columns([1,3])
+
         with col1:
             try:
                 image_url = product['image'].values[0]
-                st.image(Image.open(requests.get(image_url, stream=True).raw).resize((150,150)))
+                st.image(Image.open(requests.get(image_url, stream = True).raw).resize((150,150)))
             except:
                 st.image(Image.open('images/image-not-found-icon.png').resize((150,150)))
+
         with col2:
             st.success(product['product_name'].values[0])
-            col3,col4=st.columns(2)
+            col3,col4 = st.columns(2)
+
             with col3:
                 try:
                     st.code("Price: " + "{:,}₫".format(int(product['price'])))
@@ -115,28 +130,35 @@ def app():
                     st.code("Price: ")
             with col4:
                 st.code("Rating: " + str(product['rating'].values[0])+" ★")
+
         with st.expander("Description: "):
             st.write(product['description'].values[0])
+
         with st.expander("Setting"):
-            similar = st.slider('Select the maximum number of products similar to the above that you want the system to recommend (from 1 to 50)', 1, 15, 6)
+            similar = st.slider('Select the maximum number of products similar to the above that you want the system to recommend (from 1 to 50)', 1, 25, 6)
             rating = st.slider('Select the minimum number of ratings similar to the above that you want the system to recommend (from 1 to 10)', 0, 10, 1)
+
         if st.button('Recomment'):
-            df_filter = gensim.iloc[gensim[gensim.product_id==selected_id]['suggestion'].values[0][:similar]]
-            df_filter['rate_suggestion']=gensim[gensim.product_id==selected_id]['rate_suggestion'].values[0][:similar]
-            df_filter=df_filter[df_filter['rate_suggestion']>=(0.1*rating)]
-            if df_filter.shape[0]==0:
+            df_filter = gensim.iloc[gensim[gensim.product_id == selected_id]['suggestion'].values[0][:similar]]
+            df_filter['rate_suggestion']=gensim[gensim.product_id == selected_id]['rate_suggestion'].values[0][:similar]
+            df_filter = df_filter[df_filter['rate_suggestion'] >= (0.1*rating)]
+
+            if df_filter.shape[0] == 0:
                 st.warning('No products were found according to the settings!', icon="⚠️")
             else:
                 if df_filter.shape[0]==1:
                     st.info('There is 1 product found')
                 else:
                     st.info('There are ' + str(df_filter.shape[0]) +' products found')
+
                 display_similar_products(df_filter)
     else:
         with st.expander("Setting"):
             similar2 = st.slider('Select the maximum number of products similar to the above that you want the system to recommend', 1, 15, 6)
             rating2 = st.slider('Select the minimum number of ratings similar to the above that you want the system to recommend (from 1 to 10)', 0, 10, 0)
+        
         text = st.text_input('Enter a product name to search:', value='', max_chars=None, key=None, type='default')
+
         if text != '':
             text = clean_text(text)
             text = process_emoji(text)
@@ -147,6 +169,7 @@ def app():
             df_sim=df_sim[df_sim.iloc[:,0]>=(0.1*rating2)]
             df_sim=df_sim.sort_values(0, ascending=False)           
             df_filter = gensim.iloc[df_sim.head(similar2).index]
+
             if df_filter.shape[0]==0:
                 st.warning('No products were found according to the settings!', icon="⚠️")
             else:
@@ -154,4 +177,5 @@ def app():
                     st.info('There is 1 product found')
                 else:
                     st.info('There are ' +str(df_filter.shape[0]) +' products found')
+
                 display_similar_products(df_filter)
